a03cfc77b25c13f436cd2612c6cd11d2ae623731
Gusthavo <gusthavogp123@gmail.com>
2026-06-04 12:33:31 -0300

    refactor: transformando InputField em um componente reutilizavel.

--- frontend/src/components/CardLogin.tsx
+++ frontend/src/components/CardLogin.tsx
@@ -1,5 +1,6 @@
 import imagemLogin from '../assets/login-encurta-ae.svg'
 import ConfirmButton from './ui/ConfirmBotton'
+import InputField from './ui/InputField'  
 
 function CardLogin() {
   return (
@@ -9,15 +10,15 @@ function CardLogin() {
         <p className="text-center md-text-left text-6xl font-bold text-gray-700 ">Encurta Aê</p>
         <p className="text-xl font-light text-gray-700 mt-4 md:text-sm px-1.5">Faça login para acessar seu encurtador de links favorito.</p>
         <form className="flex flex-col md:items-center gap-4 mt-6 md:w-full w-full px-2 pb-4">
-          <input type="text" placeholder="seuemail@exemplo.com" className="w-full h-10 bg-white border  border-gray-700  rounded-sm px-3" />
-          <input type="password" placeholder="************" className="w-full h-10 bg-white border border-gray-700  rounded-sm px-3 " />
+          <InputField placeholder="Digite seu email" type="email" />
+          <InputField placeholder="Digite sua senha" type="password" />
+           <div className = "w-full h-10 justify-center items-center flex pt-4">
+      <ConfirmButton textoBotao="Entrar" />
+       </div>
         </form>
         <p className= "text-center md:text-left text-xl font-light"> Ainda não fez o cadastro? {' '} 
             <span className="text-blue-600 cursor-pointer">Cadastre-se</span>
         </p>
-      <div className = "w-full h-10 justify-center items-center flex pt-4">
-      <ConfirmButton textoBotao="Entrar" />
-       </div>
     </div>
     </div>
   )
--- frontend/src/components/ui/ConfirmBotton.tsx
+++ frontend/src/components/ui/ConfirmBotton.tsx
@@ -1,4 +1,8 @@
-function ConfirmButton({ textoBotao }: { textoBotao: string }) {
+interface ConfirmButtonProps {
+    textoBotao: string;
+}
+
+function ConfirmButton({ textoBotao }: ConfirmButtonProps) {
     return (
         <button className="w-100 h-10 bg-gray-700 font-bold text-1.5xl text-white rounded-2xl hover:bg-black transition-colors duration-300">
             {textoBotao}
