9a5592e8d9bfcd3cc6f1882cea486a40f8805e58
Gusthavo <gusthavogp123@gmail.com>
2026-06-18 02:29:45 -0300

    feat: estilização do componente delete link modal, ainda necessita da logica para deletar.

--- frontend/src/components/DeleteLinkModal.tsx
+++ frontend/src/components/DeleteLinkModal.tsx
@@ -1,64 +1,92 @@
 import { Cross1Icon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import ConfirmButton from "./ui/ConfirmBotton";
+import { Link2Icon } from "@radix-ui/react-icons";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import {GlobeIcon} from "@radix-ui/react-icons";
 
 interface ModalEditExpiresAtProps {
-
-    isOpen: boolean;
-    onClose: () => void;
-    linkId: string;
-    linkEncurtado: string;
-    linkOriginal: string;
-    dataExpiracao: string;
-    
+  isOpen: boolean;
+  onClose: () => void;
+  linkId: string;
+  linkEncurtado: string;
+  linkOriginal: string;
+  dataExpiracao: string;
 }
 
+function DeleteLinkModal({
+  isOpen,
+  onClose,
+  linkId,
+  linkEncurtado,
+  linkOriginal,
+  dataExpiracao,
+}: ModalEditExpiresAtProps) {
+  const [modalDeletarLink, setModalDeletarLink] = useState(true);
 
+  if (isOpen == false) return null;
 
-function DeleteLinkModal({isOpen,onClose,linkId,linkEncurtado,linkOriginal,dataExpiracao}: ModalEditExpiresAtProps) {
+  return (
+    <div className="h-full  w-full flex justify-center items-center z-10 fixed inset-0 bg-black/50 backdrop-blur-md">
+      <div className="bg-gray-100 w-100 h-100 rounded-2xl relative flex flex-col justify-between overflow-hidden">
+        <button
+          onClick={onClose}
+          className="flex absolute top-4 right-4 p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-full transition-colors"
+        >
+          {" "}
+          <Cross1Icon />{" "}
+        </button>
 
+        <div className="w-full">
+          <p className="flex text-left mt-4 font-bold  text-3xl font-sans py-0.5 px-2 ">
+            Deletar link encurtado
+          </p>
+          <spam className="flex text-left text-xs px-2">
+            Atenção: essa ação é permanente e irreversivel. Caso deseja apenas
+            alterar a data de expiração, clique aqui.
+          </spam>
+        </div>
 
-    const [modalDeletarLink, setModalDeletarLink] = useState(true)
+        <div className="flex flex-col text-left px-2">
+          <p className="text-xs font-bold text-gray-500 uppercase tracking-wide mb-1.5">
+            Confira os Detalhes:
+          </p>
 
-    if (isOpen==false) return null;
+          <div className="flex justify-center flex flex-col bg-gray-900 rounded-2xl font-semibold font-bold text-white font-sans p-6">
+            <h1 className="break-all flex flex-row items-center gap-2">
+              {" "}
+              <Link2Icon />: {linkEncurtado}{" "}
+            </h1>
+            <h1 className="break-all flex flex-row items-center gap-2">
+                <GlobeIcon/>: {linkOriginal} 
+            </h1>
+            <h1 className="break-all flex flex-row items-center gap-2">
+              {" "}
+              <CalendarIcon />: Data de Expiração: {dataExpiracao}{" "}
+            </h1>
+          </div>
+        </div>
 
+        <div className="bottom w-full h-20 bottom flex-row px-4 ">
+          <button
+            type="button"
+            onClick={(e) => console.log("cliquei no cancelar")}
+            className="bg-white w-1/2 h-10 rounded-2xl border-2"
+          >
+            <h1 className="font-bold font-sans"> Cancelar</h1>
+          </button>
 
-    return (
-        <div className="h-full  w-full flex justify-center items-center z-10 fixed inset-0 bg-black/50 backdrop-blur-md">
-            <div className="bg-white w-100 h-100 rounded-2xl relative flex flex-col justify-between"> 
-                <button onClick={onClose}className=" absolute top-4 right-4 p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-full transition-colors">  <Cross1Icon/>  </button>
-                            <h1> aqui é o modal de deletar link</h1>
-
-                            <div>
-                                {linkEncurtado}
-                                {linkOriginal}
-                                {dataExpiracao}
-
-                        
-                            </div>
-
-
-
-
-
-                <div className="bg-pink-400 bottom w-full h-20 bottom flex-row "> 
-
-                    <button type="button" onClick={(e)=> console.log('cliquei no cancelar')} className="bg-red-700 w-1/2 h-10 rounded-2xl">
-                        <h1 className="font-bold "> Botão de Cancelar</h1>
-                    </button>
-
-                    <button type="button" onClick={(e)=> console.log('cliquei no deletar!')} className="bg-blue-700 w-1/2 h-10 rounded-2xl"> 
-                        <h1 className="font-bold "> Botão de Deletar! </h1>
-                    </button>
-
-                </div>
-            </div>
-
-
-            </div>
-
-    )
-
+          <button
+            type="button"
+            onClick={(e) => console.log("cliquei no deletar!")}
+            className="bg-gray-900 w-1/2 h-10 rounded-2xl border-2 border-gray-200 shadow-2xl shadow-white"
+          >
+            <h1 className="font-bold text-white font-sans"> Deletar </h1>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DeleteLinkModal;
